41fe23e1f88af01d0f56c18e8062e7fd648fe702
CodeAnt AI <chinmay@codeant.ai>
2024-02-05 19:54:01 +0000

    changed files

--- reflex/components/chakra/typography/__init__.py
+++ reflex/components/chakra/typography/__init__.py
@@ -7,4 +7,4 @@ from .highlight import Highlight
 from .span import Span
 from .text import Text
 
-__all__ = [f for f in dir() if f[0].isupper() or f in ("span",)]  # type: ignore
+__all__ = [f for f in dir() if f[0].isupper() or f in {"span"}]  # type: ignore
